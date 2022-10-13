0eded2f9c5ab4546234b6eaa50e38a805d6d85b3
manuelmenama <manuel.mena090589@gmail.com>
2022-10-13 17:40:51 +0200

    quasi ended

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 //DATA
-const maxNumber = 100;
+const maxNumber = 105;
 let finalOutput = null;
+
+//element
+const rowOfContainer = document.querySelector('.row');
 
 
 //contatore che conta fino a cento
@@ -8,7 +11,10 @@ for(let i = 1; i <= maxNumber; i++){
 
   //stampa un quadrato // solo dopo
   //stampa un numero progressivo all'interno // solo dopo
+  const cardCreated = document.createElement('div');
+  cardCreated.className = 'card';
 
+  
   if(!(i % 3)){
     if((!(i % 3)) && (!(i % 5))){
       finalOutput = "FizzBuzz" 
@@ -21,7 +27,9 @@ for(let i = 1; i <= maxNumber; i++){
     finalOutput = "Buzz"
     console.log(finalOutput);
   }else{
-    console.log(i);
+    finalOutput = i
   }
   
+  cardCreated.innerHTML = finalOutput
+  rowOfContainer.append(cardCreated);
 }
