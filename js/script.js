a407c82081d07bb1a9a41b007d797e3d82bb8d72
manuelmenama <manuel.mena090589@gmail.com>
2022-10-13 16:03:42 +0200

    started js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('ciao!');
+//DATA
+const maxNumber = 100;
+
+
+//contatore che conta fino a cento
+for(let i = 0; i < maxNumber; i++){
+
+  //stampa un quadrato // solo dopo
+  //stampa un numero progressivo all'interno // solo dopo
+
+  //stampiamo i numeri progressivi
+  console.log(i + 1);
+  if(!(i % 3)){
+    //se il numero è divisibile per 3
+    //stampa Fizz
+    console.log(i, 'Fizz');
+  }else if{
+    //se il numero è divisibile per 5
+      //stampa Buzz
+
+  }
+
+
+  //se è divisibile per entrambi (3 e 5)
+    //stampa FizzBuzz
+}
